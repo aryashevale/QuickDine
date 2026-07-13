3442976575ec1900183c4b4527e7baa881013ae6
aryashevale <aryashevale@gmail.com>
2026-07-13 23:59:46 +0530

    API created for Restaurants

--- server/server.ts
+++ server/server.ts
@@ -18,6 +18,20 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
 
+app.use("/api/auth", authRouter)
+
+//Global Error Handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
+    console.error("Unhandle Error:", err);
+    res.status(500).json({
+        message: err.message || "Internal Server Error",
+        stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
+    });
+
+
+
+})
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
